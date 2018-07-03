96788bc96332b724d577a8337267fc98280c7a90
CORP\volkov.vladislav <vladislav.volkov@devexpress.com>
2018-07-03 10:54:50 +0300

    publish gh-pages

--- src/app/shared/components/index.ts
+++
@@ -1,5 +0,0 @@
-
-export * from './login/login.component';
-export * from './navigation-header/navigation-header.component';
-export * from './navigation-menu/navigation-menu.component';
-export * from './cards/card.component';
